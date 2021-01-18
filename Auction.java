1434d0d48641f41518d02e52c37bf00d8081f7c3
Patricia <p.santamartamedina@gmail.com>
2021-01-18 10:09:15 +0100

    refactorizando para eliminar variables no necesarias, usando objetos anonimos

--- Auction.java
+++ Auction.java
@@ -57,18 +57,16 @@ public class Auction
     {
         Lot selectedLot = getLot(lotNumber);
         if(selectedLot != null) {
-            Bid bid = new Bid(bidder, value);
-            boolean successful = selectedLot.bidFor(bid);
-            if(successful) {
+            if(selectedLot.bidFor(new Bid(bidder, value))) {
                 System.out.println("The bid for lot number " +
                                    lotNumber + " was successful.");
             }
             else {
                 // Report which bid is higher.
-                Bid highestBid = selectedLot.getHighestBid();
+       
                 System.out.println("Lot number: " + lotNumber +
                                    " already has a bid of: " +
-                                   highestBid.getValue());
+                                   selectedLot.getHighestBid().getValue());
             }
         }
     }
